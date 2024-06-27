bbb2333bb7459c7efd6c0cddbd4e3f755d506729
Kotaro Yamada <124547687+k-yamada1108@users.noreply.github.com>
2024-06-27 12:56:35 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");{
+        System.out.println("Hello world!");
         System.out.println("テストブランチAの内容をマージ");
-
         System.out.println("ブランチBの内容をマージ");
+        System.out.println("ブランチAの内容を追加");
     }
 }
